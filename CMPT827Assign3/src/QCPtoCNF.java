50832f75db963e557fb6c1977a937000f53ef786
wandaboyer <wbkboyer@gmail.com>
2013-10-25 13:37:39 -0700

    And so it does!

---
+++ CMPT827Assign3/src/QCPtoCNF.java
@@ -0,0 +1,13 @@
+
+public class QCPtoCNF {
+
+	public QCPtoCNF() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("And so it begins...");
+	}
+
+}
