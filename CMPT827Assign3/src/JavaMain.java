22367896cafb5d21005666d9cc6f609f50b53da4
wandaboyer <wbkboyer@gmail.com>
2013-10-28 10:16:41 -0700

    I need to make sure that AMO1 is actually working....

--- CMPT827Assign3/src/JavaMain.java
+++ CMPT827Assign3/src/JavaMain.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 
 public class JavaMain {
@@ -14,13 +15,13 @@ public class JavaMain {
 		try{
 			Scanner sc = new Scanner(new File("q_10_01.qcp"));
 			probInst q_10_01 = new probInst(sc);
-			ArrayList<Integer> whichFormulas = new ArrayList<Integer>();
-			whichFormulas.add((Integer)1);
-			whichFormulas.add((Integer)2);
-			whichFormulas.add((Integer)3);
-			whichFormulas.add((Integer)4);
-			whichFormulas.add((Integer)5);
-			whichFormulas.add((Integer)6);
+			TreeSet<Integer> whichFormulas = new TreeSet<Integer>();
+			whichFormulas.add(1);
+			whichFormulas.add(2);
+			whichFormulas.add(3);
+			whichFormulas.add(4);
+			whichFormulas.add(5);
+			whichFormulas.add(6);
 			
 			QCPtoCNF converter = new QCPtoCNF();
 			converter.constructFormulas(q_10_01, "q_10_01", whichFormulas);
--- CMPT827Assign3/src/QCPtoCNF.java
+++ CMPT827Assign3/src/QCPtoCNF.java
@@ -1,43 +1,46 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class QCPtoCNF {
-	private int numVars = 0;
+	private int numVars = 0; // the problem of counting variables is harder than thought... may have to create a treeset of the integer representations of variables so that i can get the size after
 	private int numClauses = 0;
+	private TreeSet<Integer> vars;
 	
-	protected void constructFormulas (probInst problemToConvert, String name, ArrayList<Integer> whichFormulas) throws FileNotFoundException {
+	protected void constructFormulas (probInst problemToConvert, String name, TreeSet<Integer> whichFormulas) throws FileNotFoundException {
 		PrintWriter output = new PrintWriter(name+".cnf");
 		String outputString = new String();
-		outputString = createUnitClauses(problemToConvert);
+		vars = new TreeSet<Integer>();
 		
+		outputString = createUnitClauses(problemToConvert);
 		while (!whichFormulas.isEmpty()) {
-			if (whichFormulas.contains((Integer)1)) {
+			if (whichFormulas.contains(1)) {
 				outputString += ALO1(problemToConvert);
-				whichFormulas.remove((Integer)1);
+				whichFormulas.remove(1);
 			}
-			if (whichFormulas.contains((Integer)2)) {
+			if (whichFormulas.contains(2)) {
 				outputString += AMO2(problemToConvert);
-				whichFormulas.remove((Integer)2);
+				whichFormulas.remove(2);
 			}
-			if (whichFormulas.contains((Integer)3)) {
+			if (whichFormulas.contains(3)) {
 				outputString += AMO3(problemToConvert);
-				whichFormulas.remove((Integer)3);
+				whichFormulas.remove(3);
 			}
-			if (whichFormulas.contains((Integer)4)) {
+			if (whichFormulas.contains(4)) {
 				outputString += AMO1(problemToConvert);
-				whichFormulas.remove((Integer)4);
+				whichFormulas.remove(4);
 			}
-			if (whichFormulas.contains((Integer)5)) {
+			if (whichFormulas.contains(5)) {
 				outputString += ALO2(problemToConvert);
-				whichFormulas.remove((Integer)5);
+				whichFormulas.remove(5);
 			}
-			if (whichFormulas.contains((Integer)6)) {
+			if (whichFormulas.contains(6)) {
 				outputString += ALO3(problemToConvert);
-				whichFormulas.remove((Integer)6);
+				whichFormulas.remove(6);
 			}
 		}
-		outputString = "p cnf " + numVars + " " + numClauses + "\n" + outputString;
+		outputString = "p cnf " + vars.size() + " " + numClauses + "\n" + outputString;
 		output.write(outputString);
 		output.flush(); 
 		output.close();
@@ -45,12 +48,14 @@ public class QCPtoCNF {
 	
 	private String createUnitClauses(probInst problemToConvert) {
 		String outputString = new String();
+		int dummy;
 		for (int row = 0; row < problemToConvert.getN(); row++) {
 			for (int col = 0; col < problemToConvert.getN(); col++) {
 				if (problemToConvert.getProbArr()[row][col] != -1) {
-					outputString += triplesToInteger(row, col, problemToConvert.getProbArr()[row][col], problemToConvert) + " 0\n";
+					dummy = triplesToInteger(row, col, problemToConvert.getProbArr()[row][col], problemToConvert);
+					outputString += dummy + " 0\n";
 					this.numClauses++;
-					this.numVars++;
+					this.vars.add(dummy);
 				}
 			}
 		}
@@ -62,12 +67,14 @@ public class QCPtoCNF {
 	 */
 	private String ALO1(probInst problemToConvert) {
 		String outputString = new String();
+		int dummy;
 		for (int row = 0; row < problemToConvert.getN(); row++) {
 			for (int col = 0; col < problemToConvert.getN(); col++) {
 				if (problemToConvert.getProbArr()[row][col] == -1) {
 					for (int colour = 1; colour <= problemToConvert.getN(); colour++) {
-						outputString += triplesToInteger(row, col, colour, problemToConvert) + " ";
-						this.numVars++;
+						dummy = triplesToInteger(row, col, colour, problemToConvert);
+						outputString += dummy + " ";
+						vars.add(dummy);
 					}
 					outputString += "0\n";
 					this.numClauses++;
@@ -82,12 +89,14 @@ public class QCPtoCNF {
 	 */
 	private String ALO2(probInst problemToConvert) {
 		String outputString = new String();
+		int dummy;
 		for (int row = 0; row < problemToConvert.getN(); row++) {
 			for (int colour = 1; colour <= problemToConvert.getN(); colour++) {
 				for (int col = 0; col < problemToConvert.getN(); col++) {
 					if (problemToConvert.getProbArr()[row][col] == -1) {
-						outputString += triplesToInteger(row, col, colour, problemToConvert) + " ";
-						this.numVars++;
+						dummy = triplesToInteger(row, col, colour, problemToConvert);
+						outputString += dummy + " ";
+						vars.add(dummy);
 					}
 				}
 				outputString += "0\n";
@@ -103,12 +112,14 @@ public class QCPtoCNF {
 	 */
 	private String ALO3(probInst problemToConvert) {
 		String outputString = new String();
+		int dummy;
 		for (int col = 0; col < problemToConvert.getN(); col++) {
 			for (int colour = 1; colour <= problemToConvert.getN(); colour++) {
 				for (int row = 0; row < problemToConvert.getN(); row++) {
 					if (problemToConvert.getProbArr()[row][col] == -1) {
-						outputString += triplesToInteger(row, col, colour, problemToConvert) + " ";
-						this.numVars++;
+						dummy = triplesToInteger(row, col, colour, problemToConvert);
+						outputString += dummy + " ";
+						vars.add(dummy);
 					}
 				}
 				outputString += "0\n";
@@ -122,18 +133,21 @@ public class QCPtoCNF {
 	 */
 	private String AMO1(probInst problemToConvert) {
 		String outputString = new String();
+		int dummy1, dummy2;
 		for (int row = 0; row < problemToConvert.getN(); row++) {
 			for (int col = 1; col <= problemToConvert.getN(); col++) {
-				for (int col1 = 1; col1 <= problemToConvert.getN(); col1++) {
-					for (int col2 = col1; col2 <= problemToConvert.getN(); col2++) {
-						
+				for (int colour1 = 1; colour1 <= problemToConvert.getN(); colour1++) {
+					for (int colour2 = colour1; colour2 <= problemToConvert.getN(); colour2++) {
+						dummy1 = triplesToInteger(row, col, colour1, problemToConvert);
+						dummy2 = triplesToInteger(row, col, colour2, problemToConvert);
+						outputString += -dummy1 + " " + -dummy2 + " 0\n";
+						this.vars.add(dummy1);
+						this.vars.add(dummy2);
 					}
 				}
+				this.numClauses++;
 			}
 		}
-
-		
-		
 		return outputString;
 	}
 	
