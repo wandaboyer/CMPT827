826e27901c4457bb73e2e938afc84e68a0416942
wandaboyer <wbkboyer@gmail.com>
2013-11-12 23:35:35 -0800

    Final touches
    1) Added readme
    2) Cleaned up script files

--- CMPT827Assign3/src/runsatsmart.sh
+++ CMPT827Assign3/src/runsatsmart.sh
@@ -1,10 +1,9 @@
   #!/bin/bash          
-#for i in 10 16 20 24 30 32 49 
-#do  
-	for j in 3
+for i in 10 16 20 24 30 32 49 
+do  
+	for j in 1 2 3 4
 	do
-		 #minisat ./smart/q_${i}_0${j}.cnf >> ./smart/results/q_${i}_0${j}.sol
-		minisat ./smart/q_49_0${j}.cnf >> ./smart/results/q_49_0${j}.sol
-		echo "done "+q_${i}_0${j}.cnf
+		 minisat ./smart/q_${i}_0${j}.cnf > ./smart/results/q_${i}_0${j}.sol
+		echo "done " q_${i}_0${j}.cnf
 	done
- #done
+done
